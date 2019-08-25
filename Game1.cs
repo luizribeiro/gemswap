571fa3fb0f4e962287beece0affbbe2cd69eae94
Luiz Ribeiro <luizribeiro@gmail.com>
2019-08-24 22:31:22 -0400

    Imported a simple sprite

--- Game1.cs
+++ Game1.cs
@@ -6,8 +6,16 @@ namespace test
 {
     public class Game1 : Game
     {
+        const int SPRITE_FPS = 15;
+        const int SPRITE_NUM_FRAMES = 14;
+        const int SPRITE_NUM_COLUMNS = 8;
+
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        Texture2D texture;
+        int currentFrame;
+        float timer;
+        bool isJumping;
 
         public Game1()
         {
@@ -18,7 +26,9 @@ namespace test
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            currentFrame = 0;
+            timer = 0;
+            isJumping = false;
 
             base.Initialize();
         }
@@ -26,8 +36,12 @@ namespace test
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
+            texture = this.Content.Load<Texture2D>("bob-jumping");
+        }
 
-            // TODO: use this.Content to load your game content here
+        protected override void UnloadContent()
+        {
+            Content.Unload();
         }
 
         protected override void Update(GameTime gameTime)
@@ -35,16 +49,41 @@ namespace test
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            if (isJumping) {
+                timer += (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+                if (timer >= 1000f/SPRITE_FPS) {
+                    currentFrame++;
+                    if (currentFrame == SPRITE_NUM_FRAMES) {
+                        currentFrame = 0;
+                        isJumping = false;
+                    }
+                    timer = 0f;
+                }
+            }
+
+            if (GamePad.GetState(PlayerIndex.One).Buttons.A == ButtonState.Pressed) {
+                isJumping = true;
+            }
 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(new Color(225, 222, 178));
 
-            // TODO: Add your drawing code here
+            spriteBatch.Begin(samplerState: SamplerState.PointClamp);
+            spriteBatch.Draw(
+                texture,
+                position: new Vector2(320, 320),
+                sourceRectangle: new Rectangle(
+                    (currentFrame % SPRITE_NUM_COLUMNS) * 64,
+                    (currentFrame / SPRITE_NUM_COLUMNS) * 64,
+                    64,
+                    64
+                )
+            );
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
