7342b987904532e4027923d28b6944b03c908a5f
Luiz Ribeiro <luizribeiro@gmail.com>
2020-05-24 17:49:58 -0400

    Added background to BoardRenderer

--- BoardRenderer.cs
+++ BoardRenderer.cs
@@ -7,6 +7,7 @@ namespace gemswap {
     {
         GraphicsDevice graphicsDevice;
         SpriteBatch spriteBatch;
+        Texture2D backgroundTexture;
         Texture2D gemTexture;
 
         public BoardRenderer(GraphicsDevice graphicsDevice) {
@@ -16,10 +17,32 @@ namespace gemswap {
         public void LoadContent(ContentManager contentManager) {
             this.spriteBatch = new SpriteBatch(this.graphicsDevice);
             this.gemTexture = contentManager.Load<Texture2D>("gems");
+
+            int backgroundWidth = Constants.BOARD_WIDTH * Constants.GEM_WIDTH;
+            int backgroundHeight =
+                Constants.BOARD_HEIGHT * Constants.GEM_HEIGHT;
+            this.backgroundTexture = new Texture2D(
+                this.graphicsDevice,
+                backgroundWidth,
+                backgroundHeight
+            );
+            Color[] backgroundData = new Color[
+                backgroundWidth * backgroundHeight
+            ];
+            Color backgroundColor = new Color(0, 0, 0, 100);
+            for(int i = 0; i < backgroundData.Length; i++) {
+                backgroundData[i] = backgroundColor;
+            }
+            this.backgroundTexture.SetData(backgroundData);
         }
 
         public void Draw(Board board) {
             this.spriteBatch.Begin();
+            spriteBatch.Draw(
+                this.backgroundTexture,
+                new Vector2(0, 0),
+                Color.Black
+            );
             for (int x = 0; x < Constants.BOARD_WIDTH; x++) {
                 for (int y = 0; y < Constants.BOARD_HEIGHT; y++) {
                     int gem = board.getCell(x, y);
