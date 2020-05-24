2082d303d38302eca69024091a98c68a437c34cf
Luiz Ribeiro <luizribeiro@gmail.com>
2020-05-24 18:15:37 -0400

    Keep adding new rows to the board

--- Board.cs
+++ Board.cs
@@ -30,6 +30,25 @@ namespace gemswap
         public void Update(float ellapsedMilliseconds) {
             this.offset += (Constants.GEM_HEIGHT * ellapsedMilliseconds)
                 / Constants.BOARD_SPEED_ROW_PER_MS;
+
+            if (this.offset >= Constants.GEM_HEIGHT) {
+                this.offset = 0.0f;
+                this.AddNewRow();
+            }
+        }
+
+        private void AddNewRow() {
+            for (int x = 0; x < Constants.BOARD_WIDTH; x++) {
+                for (int y = 0; y < Constants.BOARD_HEIGHT - 1; y++) {
+                    this.board[x, y] = this.board[x, y + 1];
+                }
+            }
+
+            Random random = new Random();
+            for (int x = 0; x < Constants.BOARD_WIDTH; x++) {
+                this.board[x, Constants.BOARD_HEIGHT - 1] =
+                    random.Next(0, Constants.NUM_GEMS);
+            }
         }
 
         public int getCell(int x, int y) {
