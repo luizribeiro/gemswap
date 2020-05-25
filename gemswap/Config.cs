8046d02e28036d9f1885bacb68fb960831d1c9aa
Luiz Ribeiro <luizribeiro@gmail.com>
2020-05-25 15:03:20 -0400

    Allow for setting up custom configs on tests

--- gemswap/Config.cs
+++ gemswap/Config.cs
@@ -2,21 +2,21 @@ namespace gemswap
 {
     public class Config
     {
-        public int BoardWidth { get => 6; }
-        public int BoardHeight { get => 14; }
+        public virtual int BoardWidth { get => 6; }
+        public virtual int BoardHeight { get => 14; }
 
-        public int BoardInitialMinHeight { get => 2; }
-        public int BoardInitialMaxHeight { get => 8; }
+        public virtual int BoardInitialMinHeight { get => 2; }
+        public virtual int BoardInitialMaxHeight { get => 8; }
 
-        public float BoardSpeedRowPermMs { get => 10000.0f; }
+        public virtual float BoardSpeedRowPermMs { get => 10000.0f; }
 
-        public float AnimationGemFadeInMs { get => 1000.0f; }
-        public float SwapDurationMs { get => 100.0f; }
+        public virtual float AnimationGemFadeInMs { get => 1000.0f; }
+        public virtual float SwapDurationMs { get => 100.0f; }
 
-        public int GemWidth { get => 64; }
-        public int GemHeight { get => 64; }
-        public int NumGems { get => 7; }
+        public virtual int GemWidth { get => 64; }
+        public virtual int GemHeight { get => 64; }
+        public virtual int NumGems { get => 7; }
 
-        public int CursorOffsetPx { get => 8; }
+        public virtual int CursorOffsetPx { get => 8; }
     }
 }
--- tests/BoardTests.cs
+++ tests/BoardTests.cs
@@ -1,13 +1,28 @@
+using Moq;
 using NUnit.Framework;
 
 namespace gemswap.tests
 {
     public class BoardTests
     {
+        private Config SetupConfig(
+            int boardWidth = 0,
+            int boardHeight = 0
+        ) {
+            Mock<Config> mock = new Mock<Config> { CallBase = true };
+            if (boardWidth != 0) {
+                mock.Setup(c => c.BoardWidth).Returns(boardWidth);
+            }
+            if (boardHeight != 0) {
+                mock.Setup(c => c.BoardHeight).Returns(boardHeight);
+            }
+            return mock.Object;
+        }
+
         [Test]
         public void TestBottomRowContainsGems()
         {
-            Config config = new Config();
+            Config config = this.SetupConfig();
             Board board = new Board(config);
             for (int x = 0; x < config.BoardWidth; x++) {
                 Assert.AreNotEqual(
@@ -20,7 +35,7 @@ namespace gemswap.tests
         [Test]
         public void TestTopRowIsEmpty()
         {
-            Config config = new Config();
+            Config config = this.SetupConfig();
             Board board = new Board(config);
             for (int x = 0; x < config.BoardWidth; x++) {
                 Assert.AreEqual(board.getCell(x, 0), Board.EMPTY);
