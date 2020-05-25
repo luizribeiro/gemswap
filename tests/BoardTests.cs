f24b3b329c05a2fba8e424406751d6be95565f9d
Luiz Ribeiro <luizribeiro@gmail.com>
2020-05-25 15:34:34 -0400

    Test case for eliminating gems

--- tests/BoardTests.cs
+++ tests/BoardTests.cs
@@ -19,6 +19,27 @@ namespace gemswap.tests
             return mock.Object;
         }
 
+        private Board SetupBoard(Config config, int[,] board) {
+            int width = board.Length;
+            int height = board.GetLength(0);
+            Assert.AreEqual(config.BoardHeight, height);
+            Assert.AreEqual(config.BoardWidth, width);
+
+            int[,] rotatedBoard = new int[width, height];
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    rotatedBoard[x, y] = board[y, x];
+                }
+            }
+
+            return new Board(config, rotatedBoard);
+        }
+
+        private void Update(Board board, float ellapsedMilliseconds) {
+            TimerManager.Update(ellapsedMilliseconds);
+            board.Update(ellapsedMilliseconds);
+        }
+
         [Test]
         public void TestBottomRowContainsGems()
         {
@@ -46,17 +67,36 @@ namespace gemswap.tests
         public void TestSwapping()
         {
             Config config = this.SetupConfig(boardWidth: 2, boardHeight: 1);
-            Board board = new Board(config, new[,] {{1}, {2}});
+            Board board = this.SetupBoard(config, new[,] {{1, 2}});
 
             Assert.AreEqual(1, board.getCell(0, 0));
             Assert.AreEqual(2, board.getCell(1, 0));
 
             board.Swap();
-            TimerManager.Update(config.SwapDurationMs);
-            board.Update(config.SwapDurationMs);
+            this.Update(board, config.SwapDurationMs);
 
             Assert.AreEqual(2, board.getCell(0, 0));
             Assert.AreEqual(1, board.getCell(1, 0));
+        }
+
+        [Test]
+        public void TestEliminatingGems()
+        {
+            Config config = this.SetupConfig(boardWidth: 3, boardHeight: 1);
+            Board board = this.SetupBoard(config, new[,] {{1, 1, 1}});
+
+            Assert.AreEqual(1, board.getCell(0, 0));
+            Assert.AreEqual(1, board.getCell(1, 0));
+            Assert.AreEqual(1, board.getCell(2, 0));
+
+            // trigger the elimination
+            this.Update(board, 0);
+            // wait for it to finish
+            this.Update(board, config.SwapDurationMs);
+
+            Assert.AreEqual(-1, board.getCell(0, 0));
+            Assert.AreEqual(-1, board.getCell(1, 0));
+            Assert.AreEqual(-1, board.getCell(2, 0));
         }
     }
 }
