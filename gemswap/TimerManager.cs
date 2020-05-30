75e2fbe0f4dc91212a993815ebe91547a7c8f06b
Luiz Ribeiro <luizribeiro@gmail.com>
2020-05-30 00:11:32 -0400

    Fixed lint warnings on TimerManager.cs

--- gemswap/TimerManager.cs
+++ gemswap/TimerManager.cs
@@ -1,19 +1,23 @@
-namespace GemSwap {
+namespace GemSwap
+{
     using System;
     using System.Collections.Generic;
 
-    public class TimerManager {
-        private static List<Timer> timers = new List<Timer>();
+    public class TimerManager
+    {
+        private static readonly List<Timer> Timers = new List<Timer>();
 
-        public static void AddTimer(Timer timer) {
-            TimerManager.timers.Add(timer);
+        public static void AddTimer(Timer timer)
+        {
+            TimerManager.Timers.Add(timer);
         }
 
         public static Timer AddTimer(
             float durationMilliseconds,
             float delayMilliseconds = 0.0f,
             Action? onDoneCallback = null
-        ) {
+        )
+        {
             Timer timer = new Timer(
                 durationMilliseconds: durationMilliseconds,
                 delayMilliseconds: delayMilliseconds,
@@ -23,20 +27,24 @@ namespace GemSwap {
             return timer;
         }
 
-        public static void Update(float ellapsedMilliseconds) {
-            foreach (Timer timer in TimerManager.timers) {
+        public static void Update(float ellapsedMilliseconds)
+        {
+            foreach (Timer timer in TimerManager.Timers)
+            {
                 timer.Update(ellapsedMilliseconds);
             }
 
-            TimerManager.timers.RemoveAll(t => !t.IsActive());
+            TimerManager.Timers.RemoveAll(t => !t.IsActive());
         }
 
-        public static int GetNumTimers() {
-            return TimerManager.timers.Count;
+        public static int GetNumTimers()
+        {
+            return TimerManager.Timers.Count;
         }
 
-        public static void ClearAll() {
-            TimerManager.timers.Clear();
+        public static void ClearAll()
+        {
+            TimerManager.Timers.Clear();
         }
     }
 }
